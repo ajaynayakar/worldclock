cbdcd993f7e71b52acf0540475fdda5126cdd7f7
ajaynayakar <58699383+ajaynayakar@users.noreply.github.com>
2020-09-27 12:49:05 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+import ValidatedLoginForm from "./ValidatedLoginForm";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>World Clock</h1>
+      <ValidatedLoginForm />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
+
